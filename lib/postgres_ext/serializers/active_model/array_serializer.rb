08880dcb8a59ed3cd3ce6e00d014c835615ff69d
David J. Brenes <davidjbrenes@gmail.com>
2016-02-24 13:34:08 +0100

    Task #1: Globalize attributes support
    If the atttribute is detected as translated through globalize we include the current translation and query the translation table

--- lib/postgres_ext/serializers/active_model/array_serializer.rb
+++ lib/postgres_ext/serializers/active_model/array_serializer.rb
@@ -44,6 +44,9 @@ module PostgresExt::Serializers::ActiveModel
           relation_query = relation_query.select Arel::Nodes::As.new Arel.sql(serializer_class.send("#{name}__sql")), Arel.sql(name.to_s)
         elsif klass.respond_to? "#{name}__sql"
           relation_query = relation_query.select Arel::Nodes::As.new Arel.sql(klass.send("#{name}__sql")), Arel.sql(name.to_s)
+        elsif klass.respond_to?(:translated?) && klass.translated?(name)
+          relation_query = relation_query.with_translations(Globalize.locale)
+          relation_query = relation_query.select(klass::Translation.arel_table[name])
         else
           relation_query = relation_query.select(relation_query_arel[name])
         end
